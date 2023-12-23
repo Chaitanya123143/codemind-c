8fe6ae46a4006da438a8c6815a370fa97731596b
Chaitanya123143 <147373155+Chaitanya123143@users.noreply.github.com>
2023-12-23 20:55:41 +0530

    #include<stdio.h> int main() {     int b;     scanf("%d",&b);     float d,h,g,c;     if(b<=10000)     {     d=0.80;     h=0.20;     }     else if (b<=20000)     {         d=0.90;         h=0.25;     }     else     {         d=0.95;         h=0.30;     }     c=d*b+h*b;     g=b+c;     printf("%.2f",g); }

--- Gross_Salary.c
+++ Gross_Salary.c
@@ -1,25 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int a;
-    scanf("%d",&a);
+    int b;
+    scanf("%d",&b);
     float d,h,g,c;
-    if(a<=10000)
+    if(b<=10000)
     {
-        d=0.80;
-        h=0.20;
+    d=0.80;
+    h=0.20;
     }
-    else if(a<=20000)
+    else if (b<=20000)
     {
         d=0.90;
         h=0.25;
     }
-    else
+    else 
     {
         d=0.95;
         h=0.30;
     }
-    c=d*a+h*a;
-    g=a+c;
+    c=d*b+h*b;
+    g=b+c;
     printf("%.2f",g);
 }
